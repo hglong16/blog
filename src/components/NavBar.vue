15e743e92b004163090f9a38b31b975f89d67631
hglong16 <intihad.vuong@gmail.com>
2023-05-17 00:11:29 +0700

    NavBar simple done -> Need make it complex

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,5 +11,22 @@ import { isDark } from "~/logics";
       <img v-show="isDark" src="/logo-dark.svg?url" alt="logo" />
       <img v-show="!isDark" src="/logo.svg?url" alt="logo" />
     </RouterLink>
+    <nav class="nav">
+      <div class="spacer" />
+      <div class="right">
+        <router-link to="posts" title="Blog">
+          <span class="lt-md:hidden">Blog</span>
+          <div i-ri-article-line md:hidden />
+        </router-link>
+        <router-link to="/projects" title="Projects">
+          <span class="lt-md:hidden">Projects</span>
+          <div i-ri-lightbulb-line md:hidden />
+        </router-link>
+        <a href="https://github.com/hglong16" target="_blank" title="Github">
+          <div i-uil-github-alt />
+        </a>
+        <toggle-theme />
+      </div>
+    </nav>
   </header>
 </template>

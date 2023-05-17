e9136d599c012347c52fa37be628c6836f71f8d2
hglong16 <intihad.vuong@gmail.com>
2023-05-17 12:10:31 +0700

    Complete Footer

---
+++ pages/index.md
@@ -0,0 +1,7 @@
+--- 
+title: hglong16 - Hoang Long
+description: hglong16(Hoang Long)'s Portfolio
+plum: true
+--- 
+
+Hello, World
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import '@unocss/reset/tailwind.css'
-import 'uno.css'
+import "@unocss/reset/tailwind.css";
+import "./styles/main.css";
+import "./styles/prose.css";
+import "uno.css";
 
-import autoRoutes from 'pages-generated'
-import NProgress from 'nprogress'
-import { ViteSSG } from 'vite-ssg'
-import dayjs from 'dayjs'
-import LocalizedFormat from 'dayjs/plugin/localizedFormat.js'
-import App from "~/App.vue"
+import autoRoutes from "pages-generated";
+import NProgress from "nprogress";
+import { ViteSSG } from "vite-ssg";
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat.js";
+import App from "./App.vue";
 
 const routes = autoRoutes.map((i) => {
   return {
     ...i,
-    alias: i.path.endsWith('/')
-      ? `${i.path}index.html`
-      : `${i.path}.html`,
-  }
-})
+    alias: i.path.endsWith("/") ? `${i.path}index.html` : `${i.path}.html`,
+  };
+});
 
 export const createApp = ViteSSG(
   App,
@@ -23,29 +23,28 @@ export const createApp = ViteSSG(
     routes,
     scrollBehavior(to, from, savedPosition) {
       if (savedPosition) {
-        document.body.scrollTo(savedPosition)
-        return savedPosition
-      }
-      else {
+        document.body.scrollTo(savedPosition);
+        return savedPosition;
+      } else {
         document.body.scrollTo({
           top: 0,
-        })
+        });
         return {
           top: 0,
-        }
+        };
       }
     },
   },
   ({ router, isClient }) => {
-    dayjs.extend(LocalizedFormat)
+    dayjs.extend(LocalizedFormat);
 
     if (isClient) {
       router.beforeEach(() => {
-        NProgress.start()
-      })
+        NProgress.start();
+      });
       router.afterEach(() => {
-        NProgress.done()
-      })
+        NProgress.done();
+      });
     }
-  },
-)
+  }
+);
